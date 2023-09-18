45bb132d923006007be0e47028a3a152f4323d26
LMurphy186232 <murphyl@caryinstitute.org>
2023-09-18 14:50:44 -0500

    Second version of NCI climate effect local differentiation

--- TestNCIDoubleLocalDiffClimateGrowth.h
+++ TestNCIDoubleLocalDiffClimateGrowth.h
@@ -5,7 +5,7 @@
 #define TestNCIDoubleNoLocalDiffClimateGrowth_H
 #include <fstream>
 /**
- * Tests a configuration of clNCIMasterGrowth.
+ * Tests a configuration of clNCIMasterGrowth and clNCIMasterMortality.
  */
 
 /**
@@ -15,5 +15,19 @@
  * @return Filename written.
  */
 const char* WriteNCIDoubleLocalDiffClimateGrowthXMLFile1();
+
+/**
+ * Writes a 1-year timestep file. This contains 8 species. NCI growth
+ * (auto-height) is assigned to saplings and adults of Species 2, 3, 4,
+ * 5, 6, and 7.
+ * @return Filename written.
+ */
+const char* WriteNCIDoubleLocalDiffClimateGrowthXMLFile2();
+
+/**
+ * Writes common parameter file stuff.
+ * @param oOut File stream to write to.
+ */
+void WriteNCIDoubleLocalCommon(std::fstream &oOut);
 //---------------------------------------------------------------------------
 #endif // TestNCIDoubleLocalDiffClimateGrowth_H
